ce53ec62f7247208e0ef2fc78faf638d8214b24e
Jukka Eerikäinen <jukkae@iki.fi>
2018-10-04 12:27:15 +0300

    SFX on coin pickup

---
+++ Assets/PlaySoundOnPickup.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySoundOnPickup : MonoBehaviour {
+    public AudioClip collectSound;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Player") || other.gameObject.CompareTag("MainCamera")) // TODO clear up tag use
+        {
+            AudioSource.PlayClipAtPoint(collectSound, transform.position);
+        }
+    }
+}
