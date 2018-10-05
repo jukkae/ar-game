78c76908bb26c4a7ab92a703894e80cf1618162b
Jukka Eerikäinen <jukkae@iki.fi>
2018-10-05 11:42:12 +0300

    add particles for coin pickup

--- Assets/PlaySoundOnPickup.cs
+++ Assets/PlaySoundOnPickup.cs
@@ -4,11 +4,21 @@ using UnityEngine;
 
 public class PlaySoundOnPickup : MonoBehaviour {
     public AudioClip collectSound;
+    private ParticleSystem p;
+
+    private void Start()
+    {
+        p = GetComponentInChildren<ParticleSystem>();
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.CompareTag("Player") || other.gameObject.CompareTag("MainCamera")) // TODO clear up tag use
         {
+            p.transform.parent = null;
+            p.Emit(250);
+            Destroy(p, 5f);
+
             AudioSource.PlayClipAtPoint(collectSound, transform.position);
         }
     }
